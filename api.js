9ca1ffc0a97518c6e4ed77b3430365806438f0a2
LimaGustav <lima.gustavo100@hotmail.com>
2022-02-03 11:20:45 -0300

    Finalizado

--- api.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-const api = axios.create({
-    baseURL: 'https://moderationapi.com/api/v1/moderation/text'
-})
-
-export default api;
--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const api = axios.create({
 
 const Discord = require('discord.js')
 
+var id = '';
+var cont = 1;
+
 const bot = new Discord.Client({
     intents: [
         Discord.Intents.FLAGS.GUILDS,
@@ -13,16 +16,16 @@ const bot = new Discord.Client({
     ]
 });
 
-const tokenBot = 'OTM4NDEyMTAxMDk2MDU0Nzg1.Yfp6Xw.VJ1sKIgT7uEe11A1_XFBO9rXrXY';
+const tokenBot = 'OTM4NDEyMTAxMDk2MDU0Nzg1.Yfp6Xw.Pz9Ad_OcJS0iTe8DE8kBQ0bjj8o';
 
 bot.login(tokenBot);
 bot.on('ready', () => {
     console.log('estou pronto para ser usado')
 })
 
-bot.on('message', msg => {
+bot.on('message', async msg => {
 
-    const token1 = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxZmE2YWQ5ZmRjNGRmMDAwOTk1NGM4MCIsInVzZXJJZCI6IjYxZmE2YWM1ZmRjNGRmMDAwOTk1NGM3ZSIsImlhdCI6MTY0MzgwODkzNH0.gRwXDJMw4AJ4rkVS9HPS8SACMbK-MObJaiLHLY33V34'
+    const token1 = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxZmJkZjAzNmNjNGY0MDAwOWJjZjJhOCIsInVzZXJJZCI6IjYxZmJkZWVmNmNjNGY0MDAwOWJjZjJhNiIsImlhdCI6MTY0Mzg5NjU3OX0.3g68SalFBazcx40RW8aare3U3Nx6G-O2XaiUygYEu7w'
 
     console.log(msg.content)
     api.post(' ',{
@@ -34,9 +37,30 @@ bot.on('message', msg => {
         }
     }
     ).then(response => {
-        console.log(response.data)
+        var channelMsg = "_Mensagem apagada por apresentar conteúdo inapropriado_"
+        var privateMsg = `*${msg.author.username}* Atenção! A mensagem que você enviou no canal ${msg.channel.name} foi apagada por ser ofensiva. Caso persista, você será kickado`
+
         if (response.data.toxicity.label_scores.TOXICITY > 0.70) {
-            await fucntion ()
+
+            if (msg.author.id !== id) id = msg.author.id
+            else cont = cont+1
+
+            if (cont >= 3) {
+                console.log(msg.member.kickable())
+                msg.member.kick()
+                msg.delete(10)
+                .then(msg.channel.send(channelMsg))
+            }
+            
+            else {
+                msg.delete(10)
+                .then(msg.channel.send(channelMsg))
+                .then(msg.author.send(privateMsg))
+            }
+        }
+        if (response.data.profanity.error !== undefined && msg.content != channelMsg) {
+            var avisoIngles = `_Somente mensagem em inglês no canal ${msg.channel.name}!!_`
+            msg.delete(10).then(msg.author.send(avisoIngles))
         }
     }).catch(erro => console.log(erro))
 })
